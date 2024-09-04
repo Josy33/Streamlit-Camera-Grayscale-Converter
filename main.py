6597375b2b3d84ea962e0a57939eda77b03b4682
ardit <ephraimjosiahotto@gmail.com>
2024-09-04 06:30:11 +0100

    Converting to Grayscale image, receiving an error

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ from PIL import Image
 # Start the camera
 camera_image = st.camera_input("Camera")
 
+# Create a pillow instance
+img = Image.open(camera_image)
+
+# Convert the pillow img to grayscale
+gray_img = img.convert("L")
+
+# Render the grayscale image to the webpage
+st.image(gray_img)
 
