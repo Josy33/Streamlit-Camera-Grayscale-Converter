fd7783abad1c748b15f4fd65f0051cf020997a46
ardit <ephraimjosiahotto@gmail.com>
2024-09-04 07:03:19 +0100

    Converting anyfile to grayscale

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("color to Grayscale Converter")
+
+uploaded_image = st.file_uploader("Uploaded Image")
+
 with st.expander("Start Camera"):
     # Start the camera
     camera_image = st.camera_input("Camera")
@@ -14,4 +18,14 @@ if camera_image:
 
     # Render the grayscale image to the webpage
     st.image(gray_img)
+
+# Check if the image exists meaning the user has uploaded a file
+if uploaded_image:
+    # Open the user uploaded image with PIL
+    img = Image.open(uploaded_image)
+    # Convert the image to grayscale
+    gray_uploaded_img = img.convert('L')
+    # Display the grayscale image on webpage
+    st.image(gray_uploaded_img)
+
 
