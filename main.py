2f65b6d203c3ee60c924020ca7751e28ac6badad
ardit <ephraimjosiahotto@gmail.com>
2024-09-04 06:34:37 +0100

    Converted image to grayscale without errors

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import streamlit as st
 from PIL import Image
 
+with st.expander("Start Camera"):
+    # Start the camera
+    camera_image = st.camera_input("Camera")
 
-# Start the camera
-camera_image = st.camera_input("Camera")
+if camera_image:
+    # Create a pillow instance
+    img = Image.open(camera_image)
 
-# Create a pillow instance
-img = Image.open(camera_image)
+    # Convert the pillow img to grayscale
+    gray_img = img.convert("L")
 
-# Convert the pillow img to grayscale
-gray_img = img.convert("L")
-
-# Render the grayscale image to the webpage
-st.image(gray_img)
+    # Render the grayscale image to the webpage
+    st.image(gray_img)
 
